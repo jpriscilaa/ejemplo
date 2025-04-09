7c07c3fb480d7dddb5b48b653cda114d7c7cd4b7
jpriscilaa <jazminpriscilajazmin2003@gmail.com>
2025-04-09 15:06:14 +0200

    Tutorial subido

---
+++ tutorial/src/main/java/com/ccsw/tutorial/author/AuthorRepository.java
@@ -0,0 +1,16 @@
+package com.ccsw.tutorial.author;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
+import com.ccsw.tutorial.author.model.Author;
+
+/**
+ * @author ccsw
+ *
+ */
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+    Page<Author> findAll(Pageable pageable);
+}
